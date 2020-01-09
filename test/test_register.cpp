fdc6ae2b1b3105c869b8104ef9f6dd14d970ec45
someone <someone>
2020-01-09 14:52:05 -0700

    Added first testcase for Register class

--- test/test_register.cpp
+++ test/test_register.cpp
@@ -1,2 +1,9 @@
 #include "catch.hpp"
 #include "register.h"
+
+TEST_CASE("scanItem increases the quantity of the item in register object", "[register]") {
+	Register testRegister;
+	testRegister.scanItem("coffee");
+
+	REQUIRE(testRegister.getQuantity("coffee") == 1);
+}
