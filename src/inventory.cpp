25a04b82fc32f07c3c2c00b3a394f9ea3986b06e
someone <someone>
2020-01-10 07:36:13 -0700

    Added another inventory testcase

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -1,5 +1,9 @@
 #include "inventory.h"
 
 bool Inventory::contains(string n) {
+	auto it = productList.find(n);
+	if (it == productList.end()) {
+		return false;
+	}
 	return true;
 }
--- test/test_inventory.cpp
+++ test/test_inventory.cpp
@@ -2,11 +2,17 @@
 #include "inventory.h"
 #include "product.h"
 
-TEST_CASE("contains returns true when passed a product in the inventory container", "[inventory]") {
+TEST_CASE("contains returns true when passed a product name in the inventory container", "[inventory]") {
 	Inventory testInventory;
 	testInventory.productList["rice"] = new Product("rice", 289);
 
 	REQUIRE(testInventory.contains("rice") == true);
+}
+
+TEST_CASE("contains returns false when passed a product name not in the inventory container", "[inventory]") {
+	Inventory testInventory;
+	
+	REQUIRE(testInventory.contains("coke") == false);
 }
 
 /*TEST_CASE("insert inserts a reference to a product object inside the inventory object for access", "[inventory]") {
