a1b0592bececfe13f263d6333ffa8d3b64f42adc
someone <someone>
2020-01-12 12:17:08 -0700

    Added first test case for Special class

--- src/special.h
+++ src/special.h
@@ -2,7 +2,11 @@
 #define _SPECIAL_H_
 
 class Special {
-
+private:
+	int purchaseQuantity = 0;
+public:
+	inline int getPurchaseQuantity() { return purchaseQuantity; }
+	inline void setPurchaseQuantity(int p) { purchaseQuantity = p; }
 };
 
 #endif
--- test/test_special.cpp
+++ test/test_special.cpp
@@ -1,2 +1,12 @@
 #include "catch.hpp"
 #include "special.h"
+
+TEST_CASE("Special contains members which denote the details of a special applied to a product") {
+	Special testSpecial;
+	
+	SECTION("setPurchaseQuantity sets the amount of products required to be purchased at full price") {
+		testSpecial.setPurchaseQuantity(3);
+
+		REQUIRE(testSpecial.getPurchaseQuantity() == 3);
+	}
+}
